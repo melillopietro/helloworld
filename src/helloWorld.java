2d539bbe67b76d105d95706567aad1a1565a25a9
Pietro Melillo <pietromelillo@MacBook-Pro-di-Pietro.local>
2020-07-07 12:50:59 +0200

    1.0

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,9 +1,13 @@
 public class helloWorld {
-
+    /**
+     *
+     * 
+     * @param args
+     */
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! ");
 
     }
 }
